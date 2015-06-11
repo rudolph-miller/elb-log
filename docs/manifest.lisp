73e729830bb45467ec7b57c47f1ffc54f53c447c
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2015-06-12 02:42:30 +0900

    regenerate docs.

--- docs/manifest.lisp
+++ docs/manifest.lisp
@@ -1,6 +1,6 @@
 (:docstring-markup-format :scriba
  :systems (:elb-log)
- :documents ((:title "ELB Log"
+ :documents ((:title "ELB-LOG"
               :authors ("Rudolph Miller")
               :output-format (:type :multi-html
                               :template :minima)
