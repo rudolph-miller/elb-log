217aa801656ed0ba7da2d4ce31886cacd1d57eb3
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2015-06-16 01:44:14 +0900

    remove docs/.

--- docs/manifest.lisp
+++
@@ -1,7 +0,0 @@
-(:docstring-markup-format :scriba
- :systems (:elb-log)
- :documents ((:title "ELB-LOG"
-              :authors ("Rudolph Miller")
-              :output-format (:type :multi-html
-                              :template :minima)
-              :sources ("manual.scr"))))
